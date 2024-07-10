8c73258b05acebceb4bfa3c2685aab5a72917e0d
Suchil19 <jlsuchil@gmail.com>
2024-07-10 11:18:27 -0600

    Primer Commit

---
+++ app/src/main/java/com/example/myapplication/practica.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+fun main() {
+    //Valores Enteros - Int
+    val age:Int =3
+    // long -9,223,372,036,854,775,807 a 9,2223,372,036,854,8775,807
+    val exampleLong:Long =30
+    // Valores flotantes
+    val exampleFloat:Float = 30.5f
+    //Double
+    val doubleExample:Double =3241.3123245
+     //Variables alfanumericas
+    val charExample1:Char = '@'
+    val charExample:Char = '2'
+
+    val booleanEjemplo:Boolean
+
+    print(exampleLong)
+
+}
